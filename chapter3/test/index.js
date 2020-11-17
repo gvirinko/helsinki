4acf9008dbe5f74e2b28740aaf4d8887e2491bb1
Ira Gnativ <irina.gnativ@gmail.com>
2020-11-17 13:18:46 +0100

    Added biuld folder

--- chapter3/test/index.js
+++ chapter3/test/index.js
@@ -6,6 +6,8 @@ app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 
+app.use(express.static('build'))
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
