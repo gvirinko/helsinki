fb642a4b4c9326cb3cf19182f31dc14c2c4da772
Ira Gnativ <irina.gnativ@gmail.com>
2020-12-04 12:39:29 +0100

    Solution to exercise 5.7

--- chapter5/bloglist-frontend/src/App.js
+++ chapter5/bloglist-frontend/src/App.js
@@ -112,7 +112,9 @@ const App = () => {
       <h2>Blogs:</h2>
       {
         blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+          <div>
+            <Blog key={blog.id} blog={blog} userName={user.name} />
+          </div>
         )
       }
     </div>
--- chapter5/bloglist-frontend/src/components/Blog.js
+++ chapter5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog, userName }) => {
+  const [visible, setVisible] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} - {blog.author}
+      <button onClick={() => setVisible(!visible)}>{visible ? 'Hide' : 'View'}</button>
+      {visible &&
+        <div>
+          <p>{blog.url}</p>
+          <p>Likes: <span>0</span><button>Like</button></p>
+          <p>{userName}</p>
+        </div>
+      }
+    </div>
+  )
+}
+
 
 export default Blog
