433c72df9805780cc1cf9fbaaa05c5d50b37d830
Ira Gnativ <irina.gnativ@gmail.com>
2021-01-21 22:00:13 +0100

    Solution to exercise 8.12

--- chapter8/library-frontend/src/components/Authors.js
+++ chapter8/library-frontend/src/components/Authors.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react'
 import { useQuery, useMutation } from '@apollo/client';
+import Select from 'react-select'
 
 import { ALL_AUTHORS, CHANGE_BIRTHYEAR } from '../queries'
 
 const Authors = (props) => {
-  const [name, setName] = useState('')
   const [born, setBorn] = useState('')
+  const [selectedOption, setSelectedOption] = useState(null);
+
 
   const result = useQuery(ALL_AUTHORS, {
     pollInterval: 2000
@@ -25,10 +27,14 @@ const Authors = (props) => {
 
   const submit = (event) => {
     event.preventDefault()
-    editAuthor({ variables: { name: name, setBornTo: born } })
-
-    setName('')
+    editAuthor({ variables: { name: selectedOption.value, setBornTo: born } })
     setBorn('')
+  }
+
+  let options = []
+  for (let i = 0; i < authors.length; i++) {
+    let option = { value: authors[i].name, label: authors[i].name }
+    options = options.concat(option)
   }
 
   return (
@@ -56,23 +62,17 @@ const Authors = (props) => {
       </table>
       <div>
         <h3>Set birthyear</h3>
-        <form onSubmit={submit}>
-          <div>
-            name
-          <input
-              value={name}
-              onChange={({ target }) => setName(target.value)}
-            />
-          </div>
-          <div>
-            born
-          <input
-              value={born}
-              onChange={({ target }) => setBorn(target.value)}
-            />
-          </div>
-          <button type='submit'>update author</button>
-        </form>
+      <Select
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+          options={options}
+          placeholder="Select an author..."
+      />
+      <input
+        value={born}
+        onChange={({ target }) => setBorn(target.value)}
+      />
+      <button type='submit' onClick={submit}>update author</button>
       </div>
     </div>
   )
