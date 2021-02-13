b99f4be8f10d884cf004b098f84ec36576c14a42
Ira Gnativ <irina.gnativ@gmail.com>
2021-02-13 22:44:00 +0100

    Solution to exercise 9.4

---
+++ chapter9/bmi/index.ts
@@ -0,0 +1,14 @@
+// const express = require('express');
+
+import express from 'express';
+const app = express();
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
